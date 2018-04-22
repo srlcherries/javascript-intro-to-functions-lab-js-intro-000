06b5cf4d3f917b83d34b35f4ae1cc4ef725560be
Sherry Lin <sherrylin222@gmail.com>
2018-04-22 23:17:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase() === lowercase) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!";
   }
-  else if (string.toUpperCase() === uppercase) {
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!";
   }
 }
