50c15409ec2c4c375491ca76b776fd4d0167a698
Sherry Lin <sherrylin222@gmail.com>
2018-04-22 23:10:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,4 @@ function whisper(string){
   return string.toLowerCase()
 }
 
-function logShout(string){
-  console.log string.toUpperCase()
-}
 
-function logWhisper(string){
-  console.log string.toLowerCase()
-}
