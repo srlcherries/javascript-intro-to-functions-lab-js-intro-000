e9322f32c88e7228f468b2c0597a3dc969ec7695
Sherry Lin <sherrylin222@gmail.com>
2018-04-22 23:18:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,8 @@ function sayHiToGrandma(string){
   }
   else if (string.toUpperCase() === string) {
     return "YES INDEED!";
+  } 
+  else if (string = "I love you, Grandma") {
+    return "I love you, too."
   }
 }
